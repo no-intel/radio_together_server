1e0bd40f8d451f5fdcac5601b786410b0a6b3c67
no-int <tkdduf2122@naver.com>
2025-11-28 14:19:46 +0900

    init

---
+++ src/main/java/com/noint/radio_together/RadioTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.noint.radio_together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadioTogetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noint/radio_together/RadioTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noint.radio_together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadioTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
